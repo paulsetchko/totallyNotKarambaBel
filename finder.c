aaf8056076fbfc52c17f51d8c47d92ff1ce86b1d
Paul <bronetemkinponosec@gmail.com>
2017-09-08 19:31:34 +0300

    comment style update

--- finder.c
+++ finder.c
@@ -1,9 +1,10 @@
 #include "finder.h"
 
-//can't think of a way to handle the return type since it was mentioned that
-////a failure when getting a file descriptor is a possible situation and
-////should not be treated as a programm failure
-////but for the sake of a future handling function has a return code, yup
+/* can't think of a way to handle the return type since it was mentioned that
+ * a failure when getting a file descriptor is a possible situation and
+ * should not be treated as a programm failure
+ * but for the sake of a future handling function has a return code, yup
+ */
 int md5File(void *rawData, const char *fileName) {
   int ret = -1;
   uint32_t bytes;
